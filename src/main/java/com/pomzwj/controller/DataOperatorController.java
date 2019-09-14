3204ae0c2193329a9253f186ea199085af54d093
PomZWJ <zwj@19951123>
2019-09-15 01:17:27 +0800

    添加了参数的校验

--- src/main/java/com/pomzwj/controller/DataOperatorController.java
+++ src/main/java/com/pomzwj/controller/DataOperatorController.java
@@ -8,6 +8,7 @@ import com.pomzwj.exception.DatabaseExportException;
 import com.pomzwj.exception.MessageCode;
 import com.pomzwj.officeframework.poitl.PoitlOperatorService;
 import com.pomzwj.service.IDataOperatorService;
+import com.pomzwj.utils.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -36,7 +37,22 @@ public class DataOperatorController {
         String desc = "生成word文档";
         ResponseParams responseParams = new ResponseParams();
         try {
+            //参数校验
+            if(StringUtils.isEmpty(info.getIp())){
+                throw new DatabaseExportException(MessageCode.DATABASE_IP_IS_NULL_ERROR.getCode(),MessageCode.DATABASE_IP_IS_NULL_ERROR.getMsg());
+            }
+            if(StringUtils.isEmpty(info.getPort())){
+                throw new DatabaseExportException(MessageCode.DATABASE_PORT_IS_NULL_ERROR.getCode(),MessageCode.DATABASE_PORT_IS_NULL_ERROR.getMsg());
+            }
+            if(StringUtils.isEmpty(info.getUserName())){
+                throw new DatabaseExportException(MessageCode.DATABASE_USER_IS_NULL_ERROR.getCode(),MessageCode.DATABASE_USER_IS_NULL_ERROR.getMsg());
+            }
+            if(StringUtils.isEmpty(info.getPassword())){
+                throw new DatabaseExportException(MessageCode.DATABASE_PASSWORD_IS_NULL_ERROR.getCode(),MessageCode.DATABASE_PASSWORD_IS_NULL_ERROR.getMsg());
+            }
+            //查询表信息
             List<DbTable> tableMessage = dataOperatorService.getTableName(dbKind,info);
+            //生成word文档
             poitlOperatorService.makeDoc(tableMessage);
             responseParams.setParams(true);
         } catch (Exception e) {
@@ -51,6 +67,7 @@ public class DataOperatorController {
                 responseParams.setResultMsg(MessageCode.UNKNOWN_ERROR.getMsg() + "," + e.getMessage());
             }
         }
+        log.info("desc = {},返回的数据={}",desc,JSON.toJSONString(responseParams));
         return responseParams;
     }
 
