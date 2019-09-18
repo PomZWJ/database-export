813620d0124e7b53009983c332da659c3c9c01aa
PomZWJ <zwj@19951123>
2019-09-18 08:56:49 +0800

    delete releaseJar dir

--- releaseJar/README.txt
+++
@@ -1,4 +0,0 @@
-0.需要把resources下面的application.yml文件中的import和sub-model路径改成自己本机的绝对路径(路径不能含有中文)
-1.运行run.bat
-2.访问http://localhost:9999/dbExport/web/index.html
-3.根据界面操作
--- releaseJar/run.bat
+++
@@ -1,7 +0,0 @@
-TITLE dr port 9999
-set os=package
-set osl=%os%\lib
-set osr=%os%\resources
-java -Xmx512M -Xms512M -classpath ".;%osl%;%osr%;;%os%/database-export-1.0.0-RELEASE.jar"  com.pomzwj.DatabaseExportApplication
-
-pause
