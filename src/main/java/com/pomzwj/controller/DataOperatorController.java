f0e89aafeead42f235d817fb562704fe140e62c9
1513041820@qq.com <zwj@19951123>
2019-09-24 23:35:40 +0800

    版本2的controller定义

--- src/main/java/com/pomzwj/controller/DataOperatorController.java
+++ src/main/java/com/pomzwj/controller/DataOperatorController.java
@@ -12,6 +12,7 @@ import com.pomzwj.utils.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -25,6 +26,7 @@ import java.util.*;
  */
 @Slf4j
 @Controller
+@RequestMapping(value = "/makeWord")
 public class DataOperatorController {
 
     @Autowired
@@ -32,7 +34,7 @@ public class DataOperatorController {
     @Autowired
     private PoitlOperatorService poitlOperatorService;
 
-    @RequestMapping(value = "/makeWord")
+    @RequestMapping(value = "/v1")
     public @ResponseBody ResponseParams getData(String dbKind, DbBaseInfo info) {
         String desc = "生成word文档";
         ResponseParams responseParams = new ResponseParams();
@@ -54,6 +56,60 @@ public class DataOperatorController {
             List<DbTable> tableMessage = dataOperatorService.getTableName(dbKind,info);
             //生成word文档
             poitlOperatorService.makeDoc(tableMessage);
+            responseParams.setParams(true);
+        } catch (Exception e) {
+            log.error("desc={},获取失败, 原因:{}", desc, e.getMessage(), e);
+            responseParams.setParams(null);
+            if (e instanceof DatabaseExportException) {
+                DatabaseExportException ce = (DatabaseExportException) e;
+                responseParams.setResultCode(ce.getErrorCode());
+                responseParams.setResultMsg(ce.getErrorMessage());
+            } else {
+                responseParams.setResultCode(MessageCode.UNKNOWN_ERROR.getCode());
+                responseParams.setResultMsg(MessageCode.UNKNOWN_ERROR.getMsg() + "," + e.getMessage());
+            }
+        }
+        log.info("desc = {},返回的数据={}",desc,JSON.toJSONString(responseParams));
+        return responseParams;
+    }
+
+    @RequestMapping(value = "/v2")
+    public @ResponseBody ResponseParams makeWordV2(@RequestBody Map map) {
+        String desc = "生成word文档";
+        DbBaseInfo info = new DbBaseInfo();
+        ResponseParams responseParams = new ResponseParams();
+        try {
+            //参数接收
+            String dbKind = StringUtils.getValue(map.get("dbKind"));
+            String filePath = StringUtils.getValue(map.get("filePath"));
+            info.setIp(StringUtils.getValue(map.get("ip")));
+            info.setDbName(StringUtils.getValue(map.get("dbName")));
+            info.setUserName(StringUtils.getValue(map.get("userName")));
+            info.setPassword(StringUtils.getValue(map.get("password")));
+            info.setPort(StringUtils.getValue(map.get("port")));
+            //参数校验
+            if(StringUtils.isEmpty(info.getIp())){
+                throw new DatabaseExportException(MessageCode.DATABASE_IP_IS_NULL_ERROR.getCode(),MessageCode.DATABASE_IP_IS_NULL_ERROR.getMsg());
+            }
+            if(StringUtils.isEmpty(info.getPort())){
+                throw new DatabaseExportException(MessageCode.DATABASE_PORT_IS_NULL_ERROR.getCode(),MessageCode.DATABASE_PORT_IS_NULL_ERROR.getMsg());
+            }
+            if(StringUtils.isEmpty(info.getUserName())){
+                throw new DatabaseExportException(MessageCode.DATABASE_USER_IS_NULL_ERROR.getCode(),MessageCode.DATABASE_USER_IS_NULL_ERROR.getMsg());
+            }
+            if(StringUtils.isEmpty(info.getPassword())){
+                throw new DatabaseExportException(MessageCode.DATABASE_PASSWORD_IS_NULL_ERROR.getCode(),MessageCode.DATABASE_PASSWORD_IS_NULL_ERROR.getMsg());
+            }
+            if(StringUtils.isEmpty(info.getDbName())){
+                throw new DatabaseExportException(MessageCode.DATABASE_NAME_IS_NULL_ERROR.getCode(),MessageCode.DATABASE_NAME_IS_NULL_ERROR.getMsg());
+            }
+            if(StringUtils.isEmpty(filePath)){
+                throw new DatabaseExportException(MessageCode.FILE_PATH_IS_NULL_ERROR.getCode(),MessageCode.FILE_PATH_IS_NULL_ERROR.getMsg());
+            }
+            //查询表信息
+            List<DbTable> tableMessage = dataOperatorService.getTableName(dbKind,info);
+            //生成word文档
+            poitlOperatorService.makeDoc(tableMessage,filePath);
             responseParams.setParams(true);
         } catch (Exception e) {
             log.error("desc={},获取失败, 原因:{}", desc, e.getMessage(), e);
