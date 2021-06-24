f8e14b753b118de2aa1b751026cb850ad02482c3
WQQRECLJZMSF2NX\Administrator <1513041820@qq.com>
2021-06-24 16:21:42 +0800

    多线程test

--- src/test/java/com/pomzwj/TestMy.java
+++ src/test/java/com/pomzwj/TestMy.java
@@ -1,6 +1,16 @@
 package com.pomzwj;
 
+import com.alibaba.fastjson.JSON;
+import com.pomzwj.domain.DbTable;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class TestMy {
     @Test
@@ -14,6 +24,80 @@ public class TestMy {
         do{
             System.out.println(k--);
         }while (k>1);
+    }
+    @Test
+    public void fun3(){
+        List<DbTable> dataList = new ArrayList<>();
+        for(int i=0;i<54;i++){
+            DbTable dbTable = new DbTable();
+            dbTable.setTableName(i+"");
+            dbTable.setTableComments(i+"");
+            dataList.add(dbTable);
+        }
+        int pageNum = dataList.size();
+        int pageSize = 10;
+        int totalPage = (pageNum + pageSize - 1)/pageSize;
+        boolean finishFlag = false;
+        List[] listArray = new ArrayList[totalPage];
+        for(int i=0;i<totalPage;i++){
+            List<DbTable>temp = new ArrayList<>();
+            for(int j=0;j<pageSize;j++){
+                int index = i*pageSize+j;
+                if(index>=pageNum){
+                    finishFlag = true;
+                    break;
+                }
+                temp.add(dataList.get(index));
+            }
+            listArray[i] = temp;
+            if(finishFlag){
+                break;
+            }
+        }
+        //多线程分配
+        t(listArray);
+        /*for(List list :listArray){
+            System.out.println(JSON.toJSONString(list));
+        }*/
+        for(DbTable d:dataList){
+            System.out.println(JSON.toJSONString(d));
+        }
+    }
+    public void t(List[] listArray){
+        int num = listArray.length;
+        ExecutorService es = Executors.newFixedThreadPool(4);
+        try{
+            List<Future>resultFuture = new ArrayList<>();
+            for(int i=0;i<num;i++){
+                int finalI = i;
+                Future<Boolean> submit = es.submit(new Callable<Boolean>() {
+                    @Override
+                    public Boolean call() throws Exception {
+                        try {
+                            List<DbTable> list = (List<DbTable>)listArray[finalI];
+                            for(int j=0;j<list.size();j++){
+                                DbTable dbTable = list.get(j);
+                                dbTable.setTableComments(UUID.randomUUID().toString());
+                            }
+                            return true;
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        return false;
+                    }
+                });
+                resultFuture.add(submit);
+            }
+            for(int i=0;i<resultFuture.size();i++){
+                try{
+                    Boolean o = (Boolean)resultFuture.get(i).get();
+                }catch (Exception e){
+                  e.printStackTrace();
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
