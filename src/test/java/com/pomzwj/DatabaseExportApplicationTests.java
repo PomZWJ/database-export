dd3727599a60b5a39f10bb4b9dd70da759464bac
WQQRECLJZMSF2NX\Administrator <1513041820@qq.com>
2021-06-25 17:54:55 +0800

    升级Springboot版本到2.5.0

--- src/test/java/com/pomzwj/DatabaseExportApplicationTests.java
+++ src/test/java/com/pomzwj/DatabaseExportApplicationTests.java
@@ -2,11 +2,9 @@ package com.pomzwj;
 
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-//@ExtendWith(SpringRunner.class)
+
 @SpringBootTest
 public class DatabaseExportApplicationTests {
 
