e1367cd03db8cb26e2902ce3e22cbaaa0e035105
1513041820@qq.com <zwj@19951123>
2021-06-26 20:58:52 +0800

    添加了excel导出

---
+++ src/main/java/com/pomzwj/officeframework/poi/PoiExcelOperatorService.java
@@ -0,0 +1,22 @@
+package com.pomzwj.officeframework.poi;
+
+import com.pomzwj.domain.DbTable;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 导出excel工具类
+ * @author PomZWJ
+ * @email 1513041820@qq.com
+ * @github https://github.com/PomZWJ
+ */
+@Service
+public class PoiExcelOperatorService {
+
+    public void makeXls(String dbKind, List<DbTable> tableList){
+        //XSSFWorkbook xssfWorkbook = new
+    }
+
+}
