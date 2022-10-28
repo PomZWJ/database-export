6382517be51bd06ec9dbdcdbba5d8af9bbbded4c
1513041820@qq.com <zwj@19951123>
2022-10-28 22:06:07 +0800

    升级组件版本

--- src/main/java/com/pomzwj/controller/DataExportV2Controller.java
+++ src/main/java/com/pomzwj/controller/DataExportV2Controller.java
@@ -1,6 +1,6 @@
 package com.pomzwj.controller;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.deepoove.poi.XWPFTemplate;
 import com.pomzwj.anno.DataColumnName;
 import com.pomzwj.constant.DataBaseType;
