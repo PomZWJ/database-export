82b918478314e2df788f7fdc6b40d142e410cadd
1513041820@qq.com <zwj@19951123>
2024-06-18 00:07:45 +0800

    修复代码

--- database-export-core/src/main/java/io/github/pomzwj/dbexport/core/utils/StringUtils.java
+++ database-export-core/src/main/java/io/github/pomzwj/dbexport/core/utils/StringUtils.java
@@ -8,8 +8,8 @@ package io.github.pomzwj.dbexport.core.utils;
 public class StringUtils extends org.apache.commons.lang3.StringUtils {
     /**
      * 得到object值
-     * @param value
-     * @return
+     * @param value  字符串
+     * @return 获取值
      */
     public static String getValue(Object value) {
 
@@ -22,8 +22,8 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils {
 
     /**
      * 去除字符串中的换行符
-     * @param vaa
-     * @return
+     * @param vaa 字符串
+     * @return 结果值
      */
     public static String trimLineBreak(String vaa){
         if(isBlank(vaa)){
